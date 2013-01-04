95fd73d4ec3ecf9367117fdecd22e93ce4536750
m3nt0r <m3nt0r.de+elitemod@gmail.com>
2013-01-04 04:12:12 +0100

    updated build process, now creates umake ini post compile

--- rakefile.rb
+++ rakefile.rb
@@ -2,11 +2,11 @@ require 'rake'
 require 'fileutils'
 include FileUtils
 
-# set constant values:
+# config:
 UT2004_FOLDER = File.expand_path('../')
 UMAKE_EXE = "C:/UMake.exe"
 
-# get version info
+# read version file
 buildVersion = nil
 open("version.txt") do |f|
 	buildVersion = f.read
@@ -14,13 +14,16 @@ end
 
 desc "Make a new version"
 task :default do
-	packageName = "EliteMod_v#{buildVersion}"
+	packageName = "EliteMod_#{buildVersion}"
 	target = "#{UT2004_FOLDER}/#{packageName}"
 
+	# -------------------------------------------------------
     puts "Creating Package ... #{target}"
     FileUtils.mkdir_p("#{target}/Classes");
     FileUtils.cp_r(FileList.new('*').exclude("*.rb"), target);
 	
+	# -------------------------------------------------------
+    puts "- Updating sources to refer to new package"
     files = FileList.new("#{target}/**/*.uc");
 	files.each do |file_name|
 	  originalSource = File.read(file_name)
@@ -28,5 +31,19 @@ task :default do
 	  File.open(file_name, "w") {|file| file.puts updatedSource}
 	end
 
+	# -------------------------------------------------------
+    puts "- Creating a matching UMake INI File"
+    files = FileList.new("#{target}/*.ini");
+	files.each do |file_name|
+	  originalSource = File.read(file_name)
+	  updatedSource = originalSource.gsub(/\=EliteMod/, "=#{packageName}")
+	  File.open(file_name, "w") {|file| file.puts updatedSource}
+	end
+
+	# -------------------------------------------------------
+    puts "- Running UMake"
 	system("#{UMAKE_EXE} #{target}");
+
+	# -------------------------------------------------------
+	puts "Done."
 end
